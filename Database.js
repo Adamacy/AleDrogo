be0d2c3a96ecad02d1b3436780d70d61c7b95b3c
Adamacy <a.jakubiak.srem@gmail.com>
2020-09-28 20:18:44 +0200

    Updated registration page for Wojtek

--- Database.js
+++ Database.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongo = require('mongodb');
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://Adamacy:NieInterere123@cluster0.x41no.mongodb.net/AleDrogo?retryWrites=true&w=majority"
 
@@ -13,17 +15,25 @@ app.get("/" , function(req, res){
     res.sendFile(path.join(__dirname
         +'/index.html'));
 })
-app.post('/registration', function(req,res){
-    res.send('Your username and password is:' + req.body.username + req.body.password)
-})
+
 app.listen(8080)
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
 MongoClient.connect('mongodb+srv://Adamacy:NieInterere123@cluster0.x41no.mongodb.net/AleDrogo?retryWrites=true&w=majority', function(err, db){
     if (err) throw err;
-});
-//     var dbo = db.db("mydb")
+    var dbo = db.db("mydb")
+    app.post('/registration' ,function(req, res){
+        bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+            dbo.collection('users').insertOne({'username':req.body.username, 'e-mail':req.body.email, 'password': hash}, function(err, res){
+                if (err) throw err;
+            console.log('1 Object inserted')
+            db.close()
+            })
+    })
+})
 //     dbo.createCollection('users', function(err, res){
 //         if (err) throw err;
 //         console.log("Collection Created")
 //         db.close();
 //     });
+});
